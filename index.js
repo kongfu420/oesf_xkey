fec110c414237b65cfd65fb316120577229b0ac2
root <root@vmi1638139.contaboserver.net>
2024-03-08 22:51:00 +0800

    changed id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 
 function generateKey() {
 	var  sha1 = crypto.createHash('sha1');
-	return sha1.update(genId1() + '|Rit5VJtaCz|' + genId2()).digest('base64');
+	return sha1.update(genId1() + '|WcnZYYZ3mm|' + genId2()).digest('base64');
 }
 
 
